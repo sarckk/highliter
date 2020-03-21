64e71cc5b3c96faf7718aeb9a9e9dd3fc535f046
Yong Hoon Shin <zcabyhs@ucl.ac.uk>
2020-03-21 17:53:08 +0800

    Move background script to src

---
+++ src/background/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+chrome.runtime.onInstalled.addListener(() => {});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(`Sent from: ${sender.tab}` ? 'content script' : 'the extension');
+  if (request.selectedText) {
+    chrome.storage.sync.set({ selectedText: request.selectedText }, () => {
+      console.log(`Set new selection ${request.selectedText}`);
+    });
+    sendResponse({
+      confirm: `Received text: ${request.selectedText}`
+    });
+  }
+});
